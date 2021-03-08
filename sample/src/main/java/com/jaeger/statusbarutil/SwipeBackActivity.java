fbcc595ea713017c952c97bbb09a52a89bffb87e
mochadwi <mochadwi20@gmail.com>
2021-03-08 22:58:26 +0700

    refactor(androidx): bump support library to androidx
    Signed-off-by: mochadwi <mochadwi20@gmail.com>

--- sample/src/main/java/com/jaeger/statusbarutil/SwipeBackActivity.java
+++ sample/src/main/java/com/jaeger/statusbarutil/SwipeBackActivity.java
@@ -2,8 +2,8 @@ package com.jaeger.statusbarutil;
 
 import android.graphics.Color;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v7.widget.Toolbar;
+import androidx.annotation.Nullable;
+import androidx.appcompat.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import com.jaeger.library.StatusBarUtil;
